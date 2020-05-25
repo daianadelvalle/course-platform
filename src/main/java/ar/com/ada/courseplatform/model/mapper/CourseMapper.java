70b55353eb1166190c0ce3eec57917187ce9cb1b
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2020-05-25 20:40:37 -0300

    CourseMapper

--- src/main/java/ar/com/ada/courseplatform/model/mapper/CourseMapper.java
+++ src/main/java/ar/com/ada/courseplatform/model/mapper/CourseMapper.java
@@ -1,7 +1,23 @@
 package ar.com.ada.courseplatform.model.mapper;
 
+import ar.com.ada.courseplatform.model.dto.CourseDTO;
+import ar.com.ada.courseplatform.model.entity.Course;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", uses = {})
-public interface CourseMapper extends CycleDataMapper {
+public interface CourseMapper extends CycleDataMapper<CourseDTO, Course> {
+
+    @Override
+    Course toEntity(CourseDTO dto, CycleAvoidingMappingContext context);
+
+    @Override
+    List<Course> toEntity(List<CourseDTO> dtoList, CycleAvoidingMappingContext context);
+
+    @Override
+    CourseDTO toDto(Course entity, CycleAvoidingMappingContext context);
+
+    @Override
+    List<CourseDTO> toDto(List<Course> entityList, CycleAvoidingMappingContext context);
 }
