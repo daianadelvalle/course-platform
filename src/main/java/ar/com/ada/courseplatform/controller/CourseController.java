37532f8e3669160a57d35f021c7e459ddf8cfa10
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2020-05-26 11:48:37 -0300

    all Controllers

---
+++ src/main/java/ar/com/ada/courseplatform/controller/CourseController.java
@@ -0,0 +1,53 @@
+package ar.com.ada.courseplatform.controller;
+
+import ar.com.ada.courseplatform.model.dto.CourseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    @Autowired
+    @Qualifier("courseServices")
+    private CourseServices courseServices;
+
+    @GetMapping({ "", "/" }) // localhost:8080/courses y localhost:8080/courses/ [GET]
+    public ResponseEntity getAllCourses() {
+        List<CourseDTO> all = courseServices.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping({ "/{id}", "/{id}/" }) // localhost:8080/courses/1 y localhost:8080/courses/1/ [GET]
+    public ResponseEntity getCourseById(@PathVariable Long id) {
+        CourseDTO courseById = courseServices.findCourseById(id);
+        return ResponseEntity.ok(courseById);
+    }
+
+    @PostMapping({ "", "/" }) // localhost:8080/courses y localhost:8080/courses/ [POST]
+    public ResponseEntity addNewCourse(@Valid @RequestBody CourseDTO directorDTO) throws URISyntaxException {
+        CourseDTO courseSaved = courseServices.save(courseDTO);
+        return ResponseEntity
+                .created(new URI("/courses/" + courseSaved.getId()))
+                .body(courseSaved);
+    }
+
+    @PutMapping({ "/{id}", "/{id}/" }) // localhost:8080/courses/1 y localhost:8080/courses/1/ [PUT]
+    public ResponseEntity updateDirectorById(@Valid @RequestBody CourseDTO courseDTO, @PathVariable Long id) {
+        CourseDTO courseUpdate = courseServices.updateCourse(courseDTO, id);
+        return ResponseEntity.ok(courseUpdate);
+    }
+
+    @DeleteMapping({ "/{id}", "/{id}/" }) // localhost:8080/courses/1 y localhost:8080/courses/1/ [DELETE]
+    public ResponseEntity deleteCourse(@PathVariable Long id) {
+        courseServices.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
